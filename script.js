f03aef05fab342eb6fea86679a14f6f4a822116b
Muhammad Rashid <85985115+muhammadrashid995@users.noreply.github.com>
2024-03-13 20:56:01 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// const x = 11;
+// const z = x / x;
+// const y = x / 1;
+// const v = x % 2;
+
+// if (x < 3) {
+//   console.log("it is not prime no");
+// } else if (z === 1 && y === x && v != 0) {
+//   console.log("it is  prime no");
+// } else {
+//   console.log("it is NOT prime no");
+// }
